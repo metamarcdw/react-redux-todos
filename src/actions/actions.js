9245357315715368685efc1a735b0e6ff2854d93
Marc Wood <marcdw87@gmail.com>
2018-09-08 23:25:11 -0500

    Use shorter syntax for action creators.

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -5,43 +5,35 @@ import {
   FORM_PANEL_UPDATE, LOGIN_FORM_UPDATE, CLEAR_LOGIN_FORM
 } from './types';
 
-const axios = http.create({baseURL: "https://metamarcdw.pythonanywhere.com"});
+const axios = http.create({
+  baseURL: "https://metamarcdw.pythonanywhere.com"
+});
 
-export const bearer = token => {
-  return {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  };
-};
+export const bearer = token => ({
+  headers: {
+    Authorization: `Bearer ${token}`
+  }
+});
 
-export const fetchAllTodos = token => {
-  return {
-    type: FETCH_ALL_TODOS,
-    payload: axios.get("/todo", bearer(token))
-  };
-};
+export const fetchAllTodos = token => ({
+  type: FETCH_ALL_TODOS,
+  payload: axios.get("/todo", bearer(token))
+});
 
-export const completeTodo = (id_, token) => {
-  return {
-    type: COMPLETE_TODO,
-    payload: axios.put(`/todo/${id_}`, null, bearer(token))
-  };
-};
+export const completeTodo = (id_, token) => ({
+  type: COMPLETE_TODO,
+  payload: axios.put(`/todo/${id_}`, null, bearer(token))
+});
 
-export const deleteTodo = (id_, token) => {
-  return {
-    type: DELETE_TODO,
-    payload: axios.delete(`/todo/${id_}`, bearer(token))
-  };
-};
+export const deleteTodo = (id_, token) => ({
+  type: DELETE_TODO,
+  payload: axios.delete(`/todo/${id_}`, bearer(token))
+});
 
-export const addNewTodo = (text, token) => {
-  return {
-    type: ADD_NEW_TODO,
-    payload: axios.post("/todo", { text }, bearer(token))
-  };
-};
+export const addNewTodo = (text, token) => ({
+  type: ADD_NEW_TODO,
+  payload: axios.post("/todo", { text }, bearer(token))
+});
 
 export const registerUser = (name, password) => dispatch => {
   const newUser = { name, password };
@@ -53,30 +45,24 @@ export const registerUser = (name, password) => dispatch => {
     }));
 };
 
-export const loginUser = (username, password) => {
-  return {
-    type: LOGIN_USER,
-    payload: axios.get("/login", {
-      auth: {
-        username,
-        password
-      }
-    })
-  };
-};
+export const loginUser = (username, password) => ({
+  type: LOGIN_USER,
+  payload: axios.get("/login", {
+    auth: {
+      username,
+      password
+    }
+  })
+});
 
-export const logoutUser = () => {
-  return {
-    type: LOGOUT_USER
-  };
-};
+export const logoutUser = () => ({
+  type: LOGOUT_USER
+});
 
-export const formPanelUpdate = text => {
-  return {
-    type: FORM_PANEL_UPDATE,
-    payload: { text }
-  };
-};
+export const formPanelUpdate = text => ({
+  type: FORM_PANEL_UPDATE,
+  payload: { text }
+});
 
 export const loginFormUpdate = (fieldName, fieldText) => {
   const action = {
@@ -87,8 +73,6 @@ export const loginFormUpdate = (fieldName, fieldText) => {
   return action;
 };
 
-export const clearLoginForm = () => {
-  return {
-    type: CLEAR_LOGIN_FORM
-  };
-};
+export const clearLoginForm = () => ({
+  type: CLEAR_LOGIN_FORM
+});
