ba459d995cce2815c7abff97ad95d533f8146cc1
Marc Wood <marcdw87@gmail.com>
2018-09-18 01:25:50 -0500

    Add some routes and mock data to express server.

---
+++ src/server/db.js
@@ -0,0 +1,13 @@
+export const users = [
+  { id: 1, public_id: 'uuid1', name: 'user1', password_hash: 'asdfg', admin: true },
+  { id: 2, public_id: 'uuid2', name: 'user2', password_hash: 'qwert', admin: false },
+  { id: 3, public_id: 'uuid3', name: 'user3', password_hash: 'gfdsa', admin: false }
+];
+
+export const todos = [
+  { id: 1, text: "Do a thing1", complete: true, user_id: 1 },
+  { id: 2, text: "Do a thing2", complete: false, user_id: 1 },
+  { id: 3, text: "Do a thing3", complete: false, user_id: 2 },
+  { id: 4, text: "Do a thing4", complete: false, user_id: 2 },
+  { id: 5, text: "Do a thing5", complete: false, user_id: 3 },
+]
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,43 @@
+import Joi from 'joi';
+import { users } from './db';
+import { new_user_schema } from './schemas';
+
 import express from 'express';
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.status(200).send({
-    message: 'Hello world!'
-  })
+function findItem(id, items, res) {
+  if (!id || id < 1) res.status(400).send({
+    msg: 'Bad ID given'
+  });
+  const item = items.find(i => i.id === parseInt(id));
+  if (!item) res.status(404).send({
+    msg: 'ID not found'
+  });
+  return item;
+}
+
+app.get('/user', (req, res) => {
+  if (users.length < 1) return res.status(404).send({
+    msg: 'No users found.'
+  });
+  res.send(users);
 });
 
-const PORT = 5000;
+app.get('/user/:id', (req, res) => {
+  const { id } = req.params;
+  const user = findItem(id, users, res);
+  if (user) res.send(user);
+});
+
+app.post('/user', (req, res) => {
+  const { value, error } = Joi.validate(req.body, new_user_schema);
+  if (error) return res.status(400).send({
+    msg: error.details[0].message
+  })
+  
+  res.send({ new_user: value })
+});
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
